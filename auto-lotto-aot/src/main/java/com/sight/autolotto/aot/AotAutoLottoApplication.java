cf716c69c5da72d0d4be5682e997d1ea2b8f2c34
dino.dd <dino.dd@kakaocorp.com>
2023-05-29 12:46:37 +0900

    init commit

---
+++ auto-lotto-aot/src/main/java/com/sight/autolotto/aot/AotAutoLottoApplication.java
@@ -0,0 +1,11 @@
+package com.sight.autolotto.aot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AotAutoLottoApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AotAutoLottoApplication.class, args);
+  }
+}
---
+++ auto-lotto-jit/src/main/java/com/sight/autolotto/jit/JitAutoLottoApplication.java
@@ -0,0 +1,11 @@
+package com.sight.autolotto.jit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitAutoLottoApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(JitAutoLottoApplication.class, args);
+  }
+}
